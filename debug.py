2e2eb9bbb20afa5cf00fc3d0e4934a4e4d04ba85
yang <842178881@qq.com>
2020-12-31 15:41:33 +0800

    update!

--- debug.py
+++ debug.py
@@ -1,20 +1,41 @@
 from typing import *
 
-class Solution:
-    def findRepeatNumber(self, nums: List[int]) -> int:
-        if nums is None or len(nums) == 0:
-            return None
-        n = len(nums)
-        for i in range(n):
-            while nums[i] != i:
-                if nums[nums[i]] == nums[i]:
-                    return nums[i]
-                nums[i], nums[nums[i]] = nums[nums[i]], nums[i] # 交换位置 这个写法有bug
-                
-#                 t1 = nums[i]
-#                 nums[i] = nums[t1]
-#                 nums[t1] = t1
 
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator == 0:
+            return "0"
+        res = ""
+        if numerator * denominator < 0: # 负数先变成正数处理
+            res += "-"
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+
+        if numerator == denominator:
+            return res + "1"   
+
+        int2str = [str(i) for i in range(10)]
+       
+        ans = {}
+        if numerator > denominator:
+            res += str(numerator // denominator)
+            numerator =  numerator % denominator
+            if numerator == 0: # 后面只处理小数部分
+                return res
+        else:
+            res += "0"
+        
+        res += "."
+        m = len(res) - 1
+        while numerator and numerator not in ans:
+            m += 1
+            ans[numerator] = m
+            numerator *= 10
+            res += int2str[numerator // denominator]
+            numerator %= denominator
+        
+        if numerator in ans:
+            res = res[:ans[numerator]] + "(" + res[ans[numerator]:] + ")"
+        return res   
 s = Solution()
-x = [2, 3, 1]
-print(s.findRepeatNumber(x))
+s.fractionToDecimal(1, 6)
