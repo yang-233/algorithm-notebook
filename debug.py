b52bf8088351f5aab178a7983b6ae2f4f5065770
yang <842178881@qq.com>
2021-03-08 19:54:06 +0800

    update

--- debug.py
+++ debug.py
@@ -1,31 +1,60 @@
 from typing import *
+import copy
+
+
 class Solution:
-    def treeToDoublyList(self, root: 'Node') -> 'Node':
-        def dfs(root):
-            if root is None:
-                return None, None
+    @staticmethod
+    def binary_search(a, l, r, val):
+        idx = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if a[mid] == val:
+                idx = mid
+                r = mid - 1
+            elif a[mid] > val:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return idx
+    
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return None
+        n = len(nums)
+        temp = copy.deepcopy(nums)
+        temp.sort()
+        a = []
+        for i in nums: # 离散化
+            a.append(self.binary_search(temp, 0, n - 1, i) + 1)
+        print(a)
+        class Bit:
+            @staticmethod
+            def lowbit(x):
+                return x & (-x)
+            
+            def __init__(self, n):
+                self.n = n
+                self.a = [0 for _ in range(n + 1)]
 
-            lmin, lmax = dfs(root.left)
-            rmin, rmax = dfs(root.right)
+            def query(self, x):
+                res = 0
+                while x > 0:
+                    res += self.a[x]
+                    x -= self.lowbit(x)
+                return res
 
-            l = r = root
-            root.left = root.right = None
+            def update(self, x, val=1):
+                while x <= self.n:
+                    self.a[x] += val
+                    x += self.lowbit(x)
+                    
+        bit = Bit(n)
+        res = [0 for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            res[i] = bit.query(a[i] - 1)
+            bit.update(a[i])
+        return res
 
-            if lmax is not None: # 接上左侧最大的
-                root.left = lmax
-            if lmin is not None: # 左侧最小不是自身
-                l = lmin
-
-            if rmin is not None: # 接上右侧最小
-                root.right = rmin
-            if rmax is not None: # 如果右侧最大不是本身
-                r = rmax
-            print(root.val, l.val, r.val)
-            return l, r
-        
-        lmin, rmax = dfs(root)
-        lmin.left = rmax
-        rmax.right = lmin
-        return lmin
 s = Solution()
-l = s.
+nums = [5,2,6,1]
+print(s.countSmaller(nums))
